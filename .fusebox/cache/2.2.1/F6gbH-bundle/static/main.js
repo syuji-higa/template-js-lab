3a0be8b41a33a51f7d055e0cc63d604c25948525
syuji-higa <higa@trymore-inc.jp>
2017-08-14 12:47:13 +0900

    FuseBox のキャッシュファイルを削除

--- .fusebox/cache/2.2.1/F6gbH-bundle/static/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = { contents: "'use strict';\n\nconsole.log('Hellow World!');",
-dependencies: [],
-sourceMap: {},
-headerContent: undefined,
-mtime: 1502681971000,
-devLibsRequired : undefined
-};
